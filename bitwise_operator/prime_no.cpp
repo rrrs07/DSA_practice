1fc406bbc070ec875837840b891b5d061833ac5a
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-03-14 10:42:30 +0000

    done

---
+++ bitwise_operator/prime_no.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    prime or 
+
+int n;
+cout<<"enter the value of n" <<endl;
+cin>>n;
+
+bool isPrime=1;
+
+for(int i=2; i<n; i++)
+{
+    if(n%2== 0)
+    {
+        // cout<<"not a prime number";
+
+        isPrime = 0;
+        break;
+    }
+
+    if(isPrime==0)
+    {
+        cout<<"not a prime number";
+    }
+
+    else{
+        cout<<"is prime number";
+    }
+
+
+
+   
+}
+}
