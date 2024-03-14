5ef954d358fd61508565266288f2bf12cc14e2bf
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-03-14 10:48:06 +0000

    done

--- bitwise_operator/prime_no.cpp
+++ bitwise_operator/prime_no.cpp
@@ -1,36 +1,20 @@
 #include<iostream>
 using namespace std;
-int main(){
-    
-    //prime or not
-
-int n;
-cout<<"enter the value of n" <<endl;
-cin>>n;
-
-bool isPrime=1;
-
-for(int i=2; i<n; i++)
-{
-    if(n%2== 0)
-    {
-        // cout<<"not a prime number";
-
-        isPrime = 0;
-        break;
-    }
-
-    if(isPrime==0)
-    {
-        cout<<"not a prime number";
-    }
-
-    else{
-        cout<<"is prime number";
-    }
-
-
-
-   
-}
-}
+int main(){  
+  int n, i, m=0, flag=0;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;  
+  m=n/2;  
+  for(i = 2; i <= m; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          cout<<"Number is not Prime."<<endl;  
+          flag=1;  
+          break;  
+      }  
+  }  
+  if (flag==0)  
+      cout << "Number is Prime."<<endl;  
+  return 0;  
+}  
