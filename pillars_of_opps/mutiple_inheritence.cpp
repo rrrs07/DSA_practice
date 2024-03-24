fd2097954f11386d7ec664eb65837717e8980e87
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-03-24 04:50:33 +0000

    done

---
+++ pillars_of_opps/mutiple_inheritence.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+    int age;
+    int weight;
+
+    public:
+    void bark(){
+        cout<<"Barking"<<endl;
+    }
+};
+
+class Human{
+    public:
+    string color;
+
+    public:
+    void speak(){
+        cout<<"speaking"<<endl;
+    }
+};
+
+/*Multiple inheritence*/
+class Hybrid: public Animal, public Human{
+
+};
+
+int main(){
+
+    Hybrid obj1;
+    obj1.speak();
+    obj1.bark();
+
+    return 0;
+}
