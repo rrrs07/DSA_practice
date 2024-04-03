22ecd39340d77d4d7d4768345dff2c40132b0e68
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-03 19:45:26 +0000

    done

---
+++ c++/loops/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    //fibonacciseries
+    
+    int n = 10;
+    int a=0;
+    int b=1;
+    cout<<a <<" "<<b<<" ";
+
+    for(int i=1; i<=n; i++)
+    {
+
+    int nextNumber = a+b;
+    cout<<nextNumber<<" ";
+
+    a=b;
+    b=nextNumber;
+
+    }
+}
