4c1b6ca7600b8def5ac3a0fa643220ada731de8d
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-05 18:28:43 +0000

    done

---
+++ c++/factorial_and_recursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int factorial(int n);
+
+int main(){
+    
+    // factorial : 5! = 5*4*3*2*1 = 120
+    // factorial : 5! = 1*2*3*4*5 = 120
+    int n;
+
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+
+    return 0;
+}
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
