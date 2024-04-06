a3744a15ff42278b5b3a3934cd584bdfb4ce5958
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-06 21:20:36 +0000

    done

---
+++ arrays/questions/sort_0's_&_1's.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    //sort 0's and 1's  
+
+    vector<int> arr{0,1,0,1,1,0,1,0,0,1};
+
+    int start = 0;
+    int end = arr.size()-1;
+    int i=0;
+
+    while(i<=arr.size()) {
+        if(arr[i] == 0) {
+            //swap from left
+            swap(arr[i], arr[start]);
+            i++;
+            start++;
+            
+        } else{
+            //swap from right
+            swap(arr[i], arr[end]);
+            end--;
+            i++;
+        }
+    }
+
+    //print
+    for(int i=0; i<arr.size(); i++) {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
