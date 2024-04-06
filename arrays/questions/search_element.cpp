91ef188fa89312bca57ef53eeb82c5858169068d
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-06 10:12:56 +0000

    updated

---
+++ arrays/questions/search_element.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {1,3,5,7,8};
+    int x=7;
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        if(arr[i] == x)
+        cout<<"element is present"<<endl;
+        
+    }
+    return -1;
+}
