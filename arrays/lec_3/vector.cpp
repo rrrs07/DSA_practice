26b6705d499b58bc9ce9d7f8caf6e96610d44ede
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-09 19:31:52 +0000

    done

---
+++ arrays/lec_3/vector.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    // vector<vector<int> > arr;
+
+    // vector<int> a{1, 2, 3};
+    // vector<int> b{4, 5, 6, 10};
+    // vector<int> c{7, 8, 9};
+
+    // arr.push_back(a);
+    // arr.push_back(b);
+    // arr.push_back(c);
+
+    // for(int i=0; i<arr.size(); i++){
+    //     for(int j=0; j<arr[i].size(); j++){
+    //         cout<<arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    int row = 3;
+    int col = 5;
+
+    //2d vector of size row x col
+    vector<vector<int> > arr(row,  vector<int>(col, 0));
+
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
