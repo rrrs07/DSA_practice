74b977b323f512291b15777b32c925b6e0e1f092
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-12 18:22:56 +0530

    leetcode Qno 75

--- arrays/Week3_assignments/LC_75_sortColors.cpp
+++ arrays/Week3_assignments/LC_75_sortColors.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //cannot usesorting function as mentioned in the question
-      // sort(nums.begin(),nums.end());
-
-      
+        
+       //sort(nums.begin(),nums.end());
+        
+        //in-place--- 3 pointer approach
+        
+        int l=0;
+        int m=0;
+        int h=nums.size()-1;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums[l],nums[m]);
+                l++,m++;
+            }
+            else if(nums[m] == 1){
+                m++;
+            }
+            else{
+                swap(nums[m],nums[h]);
+                h--;
+            }
+        }
+        
     }
 };
