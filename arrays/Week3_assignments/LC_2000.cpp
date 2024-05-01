c7f9ae070bed51d674c0af8569997e56e69b48a4
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-05-02 00:12:30 +0530

    reverse prefix of word

--- arrays/Week3_assignments/LC_2000.cpp
+++ arrays/Week3_assignments/LC_2000.cpp
@@ -34,19 +34,39 @@
 // 1 <= word.length <= 250
 // word consists of lowercase English letters.
 // ch is a lowercase English letter.
+
+// class Solution {
+// public:
+//     string reversePrefix(string word, char ch) {
+//         int index = -1;
+//         int n = word.size();
+//         for(int i= 0; i<n;i++){
+//             if(word[i] == ch){
+//                 index = i;
+//                 break;
+//             }
+//         }
+//         if ( index!= -1) 
+//             reverse (word.begin(),word.begin()+1+index);
+//         return word;
+    }
+};
+
+
+//2nd  similar approach
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int index = -1;
+        
         int n = word.size();
         for(int i= 0; i<n;i++){
             if(word[i] == ch){
-                index = i;
+                reverse (word.begin(),word.begin()+i+1);
                 break;
             }
         }
-        if ( index!= -1) 
-            reverse (word.begin(),word.begin()+1+index);
+        
+            
         return word;
     }
 };
