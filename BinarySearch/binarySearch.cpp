85d8fb40ce19ff6f2e07aaf64ef73263940cabc7
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-06-27 17:54:22 +0530

    binary search

---
+++ BinarySearch/binarySearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[] , int n , int target){
+    int s = 0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+
+    while(s <= e){
+        int element = arr[mid];
+        if(target == element ){
+            return mid;
+        }
+
+        else if (target < element){
+            // go left
+           e =  mid-1;
+           
+        }
+        else {
+            // go right
+           s =  mid+1;
+           mid = s + (e-s)/2;
+        }
+
+        
+    }
+    return -1;
+}
+
+
+int main(){
+    int arr[] = {10,20,30,40,50,60,70};
+    int target = 70;
+    int n = 7 ;
+     int ansIndex = binarySearch(arr , n , target);
+
+     if(ansIndex == -1){
+        cout<<"Element not found"<<endl;
+     }
+     else{
+        cout<<"Element found at Index: "<< ansIndex <<endl;
+     }
+
+     return 0;
+}
