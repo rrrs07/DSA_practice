446d058b34028d3bfc315369592d6718276f6770
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-06-29 00:49:36 +0530

    LC 852

---
+++ BinarySearch/LC852.cpp
@@ -0,0 +1,25 @@
+int peakIndexInMOuntainArray(vector<int>& arr){\
+    int n =arr.size();
+    int s= 0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+
+    while(s<e){
+        if(arr[mid] , arr[mid+1]){
+            //A wali line mein hu
+            // peak exist at right
+
+            s = mid+1;
+        }
+        else{
+            //B line pe huin
+            // ya toh peak pr hu
+
+            e = mid;
+
+        }
+        //mid update
+        mid = s+(e-s)/2;
+    }
+    return s;
+}
