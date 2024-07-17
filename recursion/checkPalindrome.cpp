06c1eedff2e0e3d58826bc4b640904a99247fc16
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-07-17 20:45:06 +0530

    check palindrome

---
+++ recursion/checkPalindrome.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+        while (start < end) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[start]) != tolower(s[end])) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+    string test = "A man, a plan, a canal: Panama";
+    if (solution.isPalindrome(test)) {
+        cout << "\"" << test << "\" is a palindrome." << endl;
+    } else {
+        cout << "\"" << test << "\" is not a palindrome." << endl;
+    }
+    return 0;
+}
