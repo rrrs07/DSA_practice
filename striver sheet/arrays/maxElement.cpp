03506df5630a5440c19c6e4026ad5aaa709f7abf
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-07-18 18:07:34 +0530

    max element in an array

---
+++ striver sheet/arrays/maxElement.cpp
@@ -0,0 +1,25 @@
+// brute force solution
+
+// sort it using any sorting method in ascending order and return the last element
+
+// Time complexity: O(n log n)
+
+//no better solution 
+
+// optimal solution
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int max = arr[0];
+        for(int i = 0; i<n;i++){
+            if(arr[i]> arr[0]){
+                max = arr[i];
+                
+            }
+                
+            }
+            return max;
+    }
+};
