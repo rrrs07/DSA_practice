a0088431ca1e6054123772d5478fd4a4ec11a0af
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-07-23 16:05:32 +0530

    rotate array

--- striver sheet/arrays/medium/rotaeArray_LC189.cpp
+++ striver sheet/arrays/medium/rotaeArray_LC189.cpp
@@ -29,4 +29,9 @@ int n = nums.size();
 
 
       //optimal sol
-      
+      k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
