958089887a02c4cddcbcd1609f1a0cd905a83960
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-07-23 16:02:55 +0530

    sorted array search

---
+++ striver sheet/arrays/easy/SortedArraySearch.cpp
@@ -0,0 +1,13 @@
+//  N = 5 , K= 3
+//  arr[] = {1,2,3,4,5}
+//  search K , if found return true
+ 
+ 
+ 
+ 
+  for(int i = 0;i<N;i++){
+           if(arr[i] == K){
+               return true;
+           }
+       }
+       return -1;
