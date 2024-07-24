e020a635bca710aa7cfd9ec1818a0eff78293737
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-07-25 01:24:00 +0530

    bubble sort

--- striver sheet/sort/bubbleSort.cpp
+++ striver sheet/sort/bubbleSort.cpp
@@ -14,3 +14,10 @@ for(int i=0;i<n-1;i++){
 
 
 
+ for(int i = n-1; i>=0;i--){
+            for(int j = 0; j<=i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr[j], arr[j+1]);
+                }
+            }
+ }
