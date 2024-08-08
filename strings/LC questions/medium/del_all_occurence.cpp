9b5f7d7a243e6932c2c59e0e711e50228b17251e
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-08-08 17:59:02 +0530

    delete all occurrence

---
+++ strings/LC questions/medium/del_all_occurence.cpp
@@ -0,0 +1,71 @@
+// 1910. Remove All Occurrences of a Substring
+
+
+// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+
+// A substring is a contiguous sequence of characters in a string.
+
+ 
+
+// Example 1:
+
+// Input: s = "daabcbaabcbc", part = "abc"
+// Output: "dab"
+// Explanation: The following operations are done:
+// - s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+// - s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+// - s = "dababc", remove "abc" starting at index 3, so s = "dab".
+// Now s has no occurrences of "abc".
+// Example 2:
+
+// Input: s = "axxxxyyyyb", part = "xy"
+// Output: "ab"
+// Explanation: The following operations are done:
+// - s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
+// - s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
+// - s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
+// - s = "axyb", remove "xy" starting at index 1 so s = "ab".
+// Now s has no occurrences of "xy".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// 1 <= part.length <= 1000
+
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.find(part) != string::npos){
+            // if inside loop, it means that part exits in s string
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+}
+
+
+
+//implementing erase function in this above code
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while (s.find(part) != string::npos) {
+            size_t pos = s.find(part);
+            s = erase(s, pos, part.length());
+        }
+        return s;
+    }
+    
+private:
+    string erase(const string& s, size_t pos, size_t length) {
+        // Create a new string by concatenating parts before and after the substring to be erased
+        string result = s.substr(0, pos) + s.substr(pos + length);
+        return result;
+    }
+};
+// Time complexity: O(n^2)
